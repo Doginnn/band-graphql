8c01fc591f73edafc2178e40fd585a6db2faec4a
Doginnn <diogenesemmanuel@gmail.com>
2024-02-26 11:55:06 -0300

    [DEL/ADD/UPD] Refatoring the API to add Factories and seed to database.

--- database/factories/MusicFactory.php
+++ database/factories/MusicFactory.php
@@ -3,21 +3,10 @@
 namespace Database\Factories;
 
 use App\Models\Album;
-use App\Models\Music;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Music>
- */
 class MusicFactory extends Factory
 {
-    protected $model = Music::class;
-
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Album;
 use App\Models\Lyric;
 use App\Models\Music;
@@ -10,22 +9,19 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+    public function run()
     {
         // Cria 10 álbuns
         $albums = Album::factory(10)->create();
 
         // Para cada álbum, cria 3 músicas associadas
         $albums->each(function ($album) {
-            $album->musics()->saveMany(Music::factory(3)->make());
-        });
+            $musics = Music::factory(3)->create(['album_id' => $album->id]);
 
-        // Para cada música, cria uma letra associada
-        Music::all()->each(function ($music) {
-            $music->lyric()->save(Lyric::factory()->make());
+            // Para cada música, cria uma letra associada
+            $musics->each(function ($music) {
+                $music->lyric()->save(Lyric::factory()->create());
+            });
         });
     }
 }
